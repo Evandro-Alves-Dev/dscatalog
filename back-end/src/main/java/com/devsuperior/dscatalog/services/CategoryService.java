ce092f7bcbe937e1083ea7e603f7630b1e468773
evandro.alves <evandroacer3@gmail.com>
2022-06-08 17:00:14 -0300

    DTO created

--- back-end/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ back-end/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,12 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryResponse;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,8 +16,11 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
+    public List<CategoryResponse> findAll() {
         var list = categoryRepository.findAll();
-        return list;
+        return list
+                .stream()
+                .map(category -> new CategoryResponse(category.getId(), category.getName()))
+                .collect(Collectors.toList());
     }
 }
